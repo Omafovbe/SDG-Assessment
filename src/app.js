46b6184e0f2998b90b14e3036eef91f136c7dca6
Omafovbe <i.omafovbe@gmail.com>
2020-04-13 02:18:21 +0100

    minor update of app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const path = require('path');
 // Import the routes module for the Estimation
 const estimationRoutes = require('./routes');
 
-const apiPort = 3000;
+const apiPort = process.env.PORT || 3000;
 const app = express();
 
 // create a write stream (in append mode)
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Morgan to log method entries and status to file on the server
-app.use(morgan(':method \t :url \t :status \t :response-time[1]ms', { stream: accessLogStream }));
+app.use(morgan(':method \t :url \t :status \t 0:response-time[0]ms', { stream: accessLogStream }));
 
 
 // Estimation API Routes
@@ -28,8 +28,10 @@ app.use('/api/v1/on-covid-19', estimationRoutes);
 
 // Request / Response log cycle
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  const data = fs.readFileSync(`${__dirname}/logs.txt`, 'utf8');
   res.set('Content-Type', 'text/plain');
-  res.sendFile(path.join(`${__dirname}/logs.txt`));
+  // res.sendFile(path.join(`${__dirname}/logs.txt`));
+  res.send(data);
 });
 
 app.listen(apiPort, () => console.log(`Server listening at http://localhost:${apiPort}`));
